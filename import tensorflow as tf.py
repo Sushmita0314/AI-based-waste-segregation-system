8f6df8256e19c491f0da19ee2f4f33e5299bc52e
Sushmita0314 <singh14sushmita@gmail.com>
2026-04-06 02:19:18 +0530

    Add files via upload

---
+++ import tensorflow as tf.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_path = "dataset/train"
+test_path = "dataset/test"
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_path,
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+test_data = test_datagen.flow_from_directory(
+    test_path,
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Conv2D(128, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(4, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_data, epochs=10, validation_data=test_data)
+
+model.save("model/waste_classifier_model.h5")
